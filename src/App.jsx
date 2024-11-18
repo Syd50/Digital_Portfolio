bd7c7bfc82831411b4295226db4dc2572cc6e7cf
Syd50 <134805159+Syd50@users.noreply.github.com>
2024-11-18 01:34:19 +0000

    Initial HomePage No 3D assets

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Home, About, Projects, Contact } from './pages';
+import Navbar from './components/Navbar';
+
+const App = () => {
+    return (
+        <main className="bg-slate-300/20">  {/*Navbar background colour*/}
+<Router>
+    <Navbar /> 
+    <Routes> {/*Different options to navigate between*/}
+        <Route path="/" element={<Home/>} />
+        <Route path="/About" element={<About/>} />
+        <Route path="/Projects" element={<Projects/>} />
+        <Route path="/Contact" element={<Contact/>} />
+    </Routes>
+</Router>
+
+        </main>
+    )
+}
+
+export default App
