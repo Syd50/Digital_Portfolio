7017b3fd6511ccf5395c895d21a43071718e7597
Syd50 <134805159+Syd50@users.noreply.github.com>
2024-12-25 04:09:18 +0000

    InitialModel

---
+++ index.js
@@ -0,0 +1,22 @@
+const buttons = document.querySelectorAll("[data-carousel-button]")
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+
+    })
+})
+
+
+
+// Wait for DOM to load OLD CODE
+
+//document.addEventListener("DOMContentLoaded", function () {
+    // Get the arrow button element
+//    const scrollArrow = document.getElementById('scrollArrow');
+
+    // Add click event to trigger scroll
+   // scrollArrow.addEventListener('click', function () {
+   //     const modelSection = document.getElementById('model');
+  //      modelSection.scrollIntoView({ behavior: 'smooth' });
+  //  });
+//})
